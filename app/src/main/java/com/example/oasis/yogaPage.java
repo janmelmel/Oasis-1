55642b8f3a08bda3b8f2d95f27677e757aeb586e
janmelmel <70429539+janmelmel@users.noreply.github.com>
2023-05-20 13:30:32 +0800

    yogaPage updated

--- app/src/main/java/com/example/oasis/yogaPage.java
+++ app/src/main/java/com/example/oasis/yogaPage.java
@@ -2,7 +2,10 @@ package com.example.oasis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class yogaPage extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class yogaPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_yoga_page);
+
+        ImageView relaxation = findViewById(R.id.relaxBtn);
+        relaxation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i  = new Intent(yogaPage.this, Relaxation.class);
+                startActivity(i);
+            }
+        });
     }
 }
