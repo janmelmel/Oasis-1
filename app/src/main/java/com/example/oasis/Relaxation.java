776910cc385f7ae280a625c09090c28c12378039
janmelmel <70429539+janmelmel@users.noreply.github.com>
2023-05-20 13:31:49 +0800

    jhgcjjnkdafs

---
+++ app/src/main/java/com/example/oasis/Relaxation.java
@@ -0,0 +1,18 @@
+package com.example.oasis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class Relaxation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relaxation);
+
+
+    }
+}
