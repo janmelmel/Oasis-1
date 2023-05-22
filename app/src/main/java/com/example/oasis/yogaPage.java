e4cb5ed74477f6832d73a82918ace05109476655
janmelmel <70429539+janmelmel@users.noreply.github.com>
2023-05-22 09:18:10 +0800

    yogaPage updated

--- app/src/main/java/com/example/oasis/yogaPage.java
+++ app/src/main/java/com/example/oasis/yogaPage.java
@@ -14,11 +14,38 @@ public class yogaPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_yoga_page);
 
-        ImageView relaxation = findViewById(R.id.relaxBtn);
+        ImageView relaxation = findViewById(R.id.relaxbtn);
         relaxation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i  = new Intent(yogaPage.this, Relaxation.class);
+                startActivity(i);
+            }
+        });
+
+        ImageView beginners = findViewById(R.id.beginnersbtn);
+        beginners.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i  = new Intent(yogaPage.this, Beginners.class);
+                startActivity(i);
+            }
+        });
+
+        ImageView flexibility = findViewById(R.id.flexbtn);
+        flexibility.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i  = new Intent(yogaPage.this, Flexibility.class);
+                startActivity(i);
+            }
+        });
+
+        ImageView stress = findViewById(R.id.stressfbtn);
+        stress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i  = new Intent(yogaPage.this, StressRelief.class);
                 startActivity(i);
             }
         });
