26fd9156fa6f970c111fb5189bec048e6ede9038
janmelmel <70429539+janmelmel@users.noreply.github.com>
2023-05-22 09:19:02 +0800

    jhkkjhg

---
+++ app/src/main/java/com/example/oasis/Beginners.java
@@ -0,0 +1,13 @@
+package com.example.oasis;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Beginners extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beginners);
+    }
+}
